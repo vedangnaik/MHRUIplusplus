6a5b03537f20453156286ee6478eacccbba84cdc
Vedang Naik <vedang.a.naik@gmail.com>
2022-02-02 01:27:41 -0500

    Moved UI closing to helpers

--- MHRUIpp/helpers.lua
+++ MHRUIpp/helpers.lua
@@ -25,6 +25,16 @@ function isInTrainingArea()
   return sdk.get_managed_singleton("snow.VillageAreaManager"):call("get__CurrentAreaNo") == 5
 end
 
+function closeUI()
+    local guiManager = sdk.get_managed_singleton("snow.gui.GuiManager")
+    guiManager:call("closeHud") -- Disable Health bar, stamina bar, quest timer, etc.
+    guiManager:call("closePartHud") -- Disable Palico and Palamute health
+    guiManager:call("closeHudSharpness") -- Disable sharpness gauge
+    -- guiManager:call("openWeaponUI") -- Weapon-specific UI e.g. Charge Blade phials
+    -- guiManager:call("setInvisibleAllGUI", false) -- Turn off 'all' GUI, too powerful i.e. removes map, pause window, everything
+    -- guiManager:call("closeAllQuestHudUI") -- Potentially useful, turns of all HUD i.e. items, everything
+end
+
 -- Temporary Logging function
 log_str = ""
 re.on_draw_ui(function() 
