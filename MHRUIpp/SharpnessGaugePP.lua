9d627b8d0e0719ae1f92c9563670009e32d1de19
Vedang Naik <vedang.a.naik@gmail.com>
2022-02-04 00:53:36 -0500

    Added sharpness gauge

---
+++ MHRUIpp/SharpnessGaugePP.lua
@@ -0,0 +1,82 @@
+require("MHRUIpp/ElementPPBase")
+
+-- This is a superclass of the ElementPPBase class
+SharpnessGaugePP = ElementPPBase:new():new("SharpnessGauge++.json", {
+    x           = 5,
+    y           = 80,
+    w           = 70,
+    borderWidth = 2,
+    borderColor = "0xAA000000",
+    textColor   = "0xFFFFFFFF",
+})
+
+SharpnessGaugePP.sharpnessColors = {
+    [0] = "0xAA2C2CC9",
+    [1] = "0xAA2C66C9",
+    [2] = "0xAA2CD1C9",
+    [3] = "0xAA2CD970",
+    [4] = "0xAAD9862C",
+    [5] = "0xAAFFFFFF",
+}
+
+function SharpnessGaugePP:draw()
+    local playerBase = getPlayer()
+    -- Get raw values.
+    local currentSharpness = playerBase:call("get_SharpnessGauge")
+    local maxSharpness = playerBase:call("get_SharpnessGaugeMax")
+    -- Get levels for color.
+    local currentLv = playerBase:call("get_SharpnessLv")
+    local currentLvGaugeLength = playerBase:call("getSharpnessLvLength", currentLv)
+    local maxLv = playerBase:call("getSharpnessLvToGauge", maxSharpness)
+    -- Get lower bound for current level.
+    for level = maxLv, currentLv, -1 do
+        maxSharpness = maxSharpness - playerBase:call("getSharpnessLvLength", level)
+    end
+    -- Get current and max sharpness w.r.t. this level.
+    currentSharpness = currentSharpness - maxSharpness
+    maxSharpness = currentLvGaugeLength
+
+    local h = self.cfg.fontSize + (textVertOffset << 1)
+    local borderOffset = self.cfg.borderWidth << 1
+
+    imgui.push_font(self.font)
+	draw.filled_rect(self.cfg.x - self.cfg.borderWidth, self.cfg.y - self.cfg.borderWidth, self.cfg.w + borderOffset, h + borderOffset, self.cfg.borderColor)
+	draw.filled_rect(self.cfg.x, self.cfg.y, self.cfg.w * (currentSharpness / maxSharpness), h, self.sharpnessColors[currentLv])
+	draw.text(
+        string.format("%d/%d", currentSharpness, maxSharpness), 
+        self.cfg.x + textHorizOffset, self.cfg.y + textVertOffset, 0xFFFFFFFF
+    )
+    imgui.pop_font()
+end
+
+function SharpnessGaugePP:drawConfigWindow()
+    self.cfgWinVisible = imgui.begin_window("Configure Sharpness Gauge++", true, 0x10120)
+	if not self.cfgWinVisible then return false end
+    
+    local changed = false;
+    changed, self.cfg.visible = imgui.checkbox("Show?", self.cfg.visible)
+    self.cfgChanged = self.cfgChanged or changed
+    imgui.new_line()
+
+    changed, self.cfg.fontSize = imgui.drag_int("Font Size", self.cfg.fontSize, 1, 2, 50)
+    self.cfgChanged = self.cfgChanged or changed
+    changed, self.cfg.x = imgui.drag_int("X position", self.cfg.x, 2, 0, 10000)
+    self.cfgChanged = self.cfgChanged or changed
+    changed, self.cfg.y = imgui.drag_int("Y position", self.cfg.y, 2, 0, 10000)
+    self.cfgChanged = self.cfgChanged or changed
+    changed, self.cfg.w = imgui.drag_int("Width", self.cfg.w, 2, 0, 10000)
+    self.cfgChanged = self.cfgChanged or changed
+    changed, self.cfg.borderWidth = imgui.drag_int("Border", self.cfg.borderWidth, 1, 0, 50)
+    self.cfgChanged = self.cfgChanged or changed
+    imgui.text("Color pickers coming soon!")
+    imgui.new_line()
+    
+    if imgui.button("Reset Defaults") then
+        self:restoreDefaults()
+        self.cfgChanged = true
+    end
+
+	imgui.end_window()
+
+    return true
+end
